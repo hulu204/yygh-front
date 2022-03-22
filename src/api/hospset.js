a1c428d722a6b215ecdb44d6a6adf42753f52fef
李聪燕 <863541923@qq.com>
2022-03-22 09:05:48 +0800

    master

--- src/api/hospset.js
+++ src/api/hospset.js
@@ -6,9 +6,34 @@ export default {
       url: `/admin/hosp/hospitalSet/findPageHospSet/${current}/${limit}`,
       method: 'post',
       data: searchObj
-      // method: 'get',
-      // params
     })
+  },
+  deleteHospSet(id) {
+    return request({
+      url: `/admin/hosp/hospitalSet/delHospSet/${id}`,
+      method: 'delete'
+    })
+  },
+  batchDelHospSet(idList) {
+    return request ({
+      url: `/admin/hosp/hospitalSet/batchDelHospSet`,
+      method: 'delete',
+      data: idList
+    })
+  },
+  lockHospSet(id,status) {
+    return request ({
+      url: `/admin/hosp/hospitalSet/lockHospSet/${id}/${status}`,
+      method: 'put'
+    })
+  },
+  saveHospSet(hospitalSet) {
+    return request ({
+      url: `/admin/hosp/hospitalSet/saveHospitalSet`,
+      method: 'post',
+      data: hospitalSet
+    })
+    
   }
   
 }
--- src/views/hospset/list.vue
+++ src/views/hospset/list.vue
@@ -10,12 +10,16 @@
             </el-form-item>
             <el-button type="primary" icon="el-icon-search" @click="getList()">查询</el-button>
         </el-form>
+
+        <!-- 工具条 -->
+        <div>
+            <el-button type="danger" size="mini" @click="removeRows()">批量删除</el-button>
+        </div>
+
         <!-- banner列表 -->
         <el-table
-        :data="list"
-        stripe
-            style="width: 100%">
-
+        :data="list" stripe style="width: 100%" @selection-change="handleSelectionChange">
+            <el-table-column type="selection" width="55"/>
             <el-table-column type="index" label="序号" width="50"/>
             <el-table-column prop="hosname" label="医院名称"/>
             <el-table-column prop="hoscode" label="医院编号"/>
@@ -25,6 +29,13 @@
             <el-table-column label="状态" width="80">
                 <template slot-scope="scope">
                         {{ scope.row.status === 1 ? '可用' : '不可用' }}
+                </template>
+            </el-table-column>
+            <el-table-column label="操作" width="280" align="center">
+                <template slot-scope="scope">
+                    <el-button type="danger" size="mini" icon="el-icon-delete" @click="removeDataById(scope.row.id)"></el-button>
+                    <el-button v-if="scope.row.status==1" type="primary" size="mini"  @click="lockHospSet(scope.row.id,0)">锁定</el-button>
+                    <el-button v-if="scope.row.status==0" type="primary" size="mini"  @click="lockHospSet(scope.row.id,1)">解锁</el-button>               
                 </template>
             </el-table-column>
         </el-table>
@@ -49,25 +60,83 @@ export default{
             limit:3,
             searchObj:{},
             list:[],
-            total:0
+            total:0,
+            multipleSelection: []
         }
     },
     created() {
         this.getList()
     },
     methods:{
+        // 批量复选框选定
+        handleSelectionChange(selection) {
+            this.multipleSelection = selection
+        },
+        // 批量删除
+        removeRows(multipleSelection) {
+            this.$confirm('此操作将永久删除医院是设置信息,是否继续?','提示',{
+                confirmButtonText: '确定',
+                cancelButtonText: '取消',
+                type: 'warning'
+                }).then(() => { //确定执行then方法
+                //调用接口
+                var idList = []
+                for (var i = 0; i < this.multipleSelection.length; i++) {
+                    var obj = this.multipleSelection[i]
+                    idList.push(obj.id)
+                }
+                hospset.batchDelHospSet(idList)
+                .then(response => {
+                    //提示
+                    this.$message({
+                        type: 'success',
+                        message: '删除成功!'
+                    })
+                    //刷新页面
+                    this.getList(1)
+                })
+            })
+        },
         // 医院设置列表
         getList(page=1) {
             this.current = page
             hospset.getHospSetList(this.current,this.limit,this.searchObj)
-            .then(response => {
-                this.list = response.data.records
-                this.total = response.data.total
-            })// 请求成功
-            .catch(error => {
-                console.log(error)
+                .then(response => {
+                    this.list = response.data.records
+                    this.total = response.data.total
+                })// 请求成功
+                .catch(error => {
+                    console.log(error)
             })//请求失败
+        },
+        // 删除医院设置
+        removeDataById(id) {
+            this.$confirm('此操作将永久删除医院是设置信息,是否继续?','提示',{
+                confirmButtonText: '确定',
+                cancelButtonText: '取消',
+                type: 'warning'
+                }).then(() => { //确定执行then方法
+                //调用接口
+                hospset.deleteHospSet(id)
+                .then(response => {
+                    //提示
+                    this.$message({
+                        type: 'success',
+                        message: '删除成功!'
+                    })
+                    //刷新页面
+                    this.getList(1)
+                })
+            })
+        },
+        // 更改状态
+        lockHospSet(id,status) {
+            hospset.lockHospSet(id,status)
+                .then(response => {
+                   this.getList(this.current)
+            })
         }
+
     }
 }
 </script>
